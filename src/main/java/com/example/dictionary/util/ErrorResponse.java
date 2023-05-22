50877aa3f6968043c08745d459e582002204854f
Rohlend <gskosh1w@gmail.com>
2023-05-22 19:43:48 +0300

    add exception handler

---
+++ src/main/java/com/example/dictionary/util/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.example.dictionary.util;
+
+public class ErrorResponse {
+    private String message;
+    private long timestamp;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/com/example/dictionary/util/PersonNotCreatedException.java
@@ -0,0 +1,8 @@
+package com.example.dictionary.util;
+
+public class PersonNotCreatedException extends RuntimeException{
+    public PersonNotCreatedException(String message) {
+        super(message);
+
+    }
+}
