f3cebce6fcc7eb31eccce5392ff9505134eaeeae
Rohlend <gskosh1w@gmail.com>
2023-06-01 23:41:15 +0300

    fix logout function

--- src/main/java/com/example/dictionary/config/SecurityConfig.java
+++ src/main/java/com/example/dictionary/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.dictionary.config;
 
 import com.example.dictionary.services.PersonDetailsService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -62,6 +64,10 @@ public class SecurityConfig {
             .and()
             .logout()
             .logoutUrl("/logout")
+                .logoutSuccessHandler(((request, response, authentication) -> {
+                    SecurityContextHolder.clearContext();
+                    response.setStatus(HttpServletResponse.SC_OK);
+                }))
             .logoutSuccessUrl("/auth/login")
                 .and()
                 .sessionManagement()
