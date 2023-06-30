5b732eb14bcc16d861bbb24a4b7ce329ac5296df
barbaratimson <cokolek.sokol2014@mail.ru>
2023-07-01 05:28:07 +0700

    Reworked user menu

---
+++ frontend/src/components/Delete.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+const Delete = () => {
+    const link = "http://localhost:8080"
+    const navigate = useNavigate()
+    let Authorization = `Bearer ${localStorage.getItem("jwt")}`
+    const handleLogout = async () => {
+        try {
+            const response = await axios.delete(`${link}/delete`,{headers:{
+            'Authorization':Authorization
+        }
+        }); 
+         localStorage.clear()
+            navigate("/register")
+            console.log(response.data)
+        } catch (error) {
+            console.error('Ошибка при получении данных словарей:', error);
+        }
+    }
+    
+    useEffect(() => {
+    }, []);
+    return (
+            <div className="logout">
+                <div className='logout-title'>Удалить аккаунт?</div>
+                <button className='button button-alert' onClick={()=>{handleLogout()}}>Delete account</button>
+                </div>
+        
+    );
+};
+
+export default Delete;
